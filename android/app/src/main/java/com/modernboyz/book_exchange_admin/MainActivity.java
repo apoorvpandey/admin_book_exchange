d5fad303c7d875d52848f993cee4fd8e001777a3
Apoorv Pandey <apoorvpandey111@gmail.com>
2020-07-27 15:44:15 +0530

    Initial commit

---
+++ android/app/src/main/java/com/modernboyz/book_exchange_admin/MainActivity.java
@@ -0,0 +1,6 @@
+package com.modernboyz.book_exchange_admin;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:book_exchange_admin/screens/admin.dart';
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Admin(),
+  ));
+}
